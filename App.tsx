a756510173ad7e2e5e7903dd85edbdbd4b780fe1
0tii <daniel.rauhut94@googlemail.com>
2024-03-04 18:18:07 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { openDatabaseSync } from 'expo-sqlite/next';
+
+const expo = openDatabaseSync('db.db');
+const db = drizzle(expo);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
